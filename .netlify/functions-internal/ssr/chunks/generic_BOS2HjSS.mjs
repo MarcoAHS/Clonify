78409fe97082d7f53ef4f06a90eee159109f3192
MarcoAHS <marco_hdz_antonio@outlook.com>
2024-06-20 20:18:37 -0600

    Unnecessary Files

--- .netlify/functions-internal/ssr/chunks/generic_BOS2HjSS.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_DJIYBpFf.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_SUCarICd.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Cl2nVf5H.mjs');
-
-export { page };
